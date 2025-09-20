6b8429af4162534e344e8cb258a3044ed4e7f802
Chanraksa Ory <OCraksa@gmail.com>
2025-09-21 00:33:36 +0700

    feat: added mobile

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KaCd9Aw3k4UmYrma4oMVaBVgFgJ7TfWAC9hiad+SQqo=\"\n}"
